576363b4ceddf826954ca3844a8493c26994ab03
Patrick <pk@hilotec.net>
2016-09-19 20:00:05 +0200

    Added getters and setters

--- src/AppBundle/Entity/UserList.php
+++ src/AppBundle/Entity/UserList.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,4 +23,55 @@ class UserList
      * @var User
      */
     private $user;
+
+    /**
+     * @ORM\OneToMany(targetEntity="UserListWord", mappedBy="userList")
+     * @var ArrayCollection
+     */
+    private $userListWords;
+
+    public function __construct()
+    {
+        $this->userListWords = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getUserListWords()
+    {
+        return $this->userListWords;
+    }
+
+    /**
+     * @param ArrayCollection $userListWords
+     */
+    public function setUserListWords($userListWords)
+    {
+        $this->userListWords = $userListWords;
+    }
 }
